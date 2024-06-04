b4b16d7ed99529ae088936498934e0ade2f9bb04
Dark Kuroe <46422003+polakbors@users.noreply.github.com>
2024-06-04 16:40:12 +0200

    Update random_number.py

--- random_number.py
+++ random_number.py
@@ -4,5 +4,8 @@ def generate_random_number():
     return random.randint(1, 100)
 
 if __name__ == "__main__":
-    random_number = generate_random_number()
-    print(f"The random number is: {random_number}")
+    while true:
+        random_number = generate_random_number()
+        print(f"The random number is: {random_number}")
+        sleep(10)
+        
